bfe054effba041e45fbbda1f28ad7d0bf0ccb1ad
onrsgr <60180086+onrsgr@users.noreply.github.com>
2022-04-12 22:27:37 +0300

    Update PersonelExceptionHandler.java

--- personel-service/src/main/java/com/onrsgr/rws/personelservice/personelmanagement/Exception/PersonelExceptionHandler.java
+++ personel-service/src/main/java/com/onrsgr/rws/personelservice/personelmanagement/Exception/PersonelExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class PersonelExceptionHandler {
-
     /*
     @ExceptionHandler(MissingHeaderInfoException.class)
     public final ResponseEntity<ErrorResponse> handleInvalidTraceIdException
